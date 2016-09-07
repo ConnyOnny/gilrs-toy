bfbeeec638bb9c54e6cbdbd9d81ef9ee97ada9d2
Constantin Berhard <git.mail.enormator@xoxy.net>
2016-09-07 21:46:11 +0200

    refactored stuff into Player struct

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,29 @@ extern crate piston_window;
 
 use piston_window::*;
 
+struct Player {
+    position: (f64,f64),
+    gamepad_id: usize,
+    deadzone: f64,
+    color: [f32;4],
+}
+
+impl Player {
+    fn control(&mut self, g : &gilrs::Gilrs) {
+        let (dx,dy) = g.gamepad(self.gamepad_id).state().left_stick;
+        self.position.0 += apply_deadzone(self.deadzone, dx as f64);
+        self.position.1 -= apply_deadzone(self.deadzone, dy as f64);
+    }
+    fn render(&self, e : &Event, w : &mut PistonWindow) {
+        w.draw_2d(e, |c, g| {
+            clear([1.0; 4], g);
+            rectangle(self.color,
+                      [0.0, 0.0, 10.0, 10.0],
+                      c.transform.trans(self.position.0,self.position.1), g);
+        });
+    }
+}
+
 fn apply_deadzone(dead : f64, mut value : f64) -> f64 {
     let s = value.signum();
     if s == -1.0 {
@@ -16,23 +39,18 @@ fn apply_deadzone(dead : f64, mut value : f64) -> f64 {
 }
 
 fn main() {
+    let window_size = (800,600);
     let deadzone = 0.2;
+    let mut players = vec![Player{position:(0.,0.),gamepad_id:0,deadzone:deadzone,color:[0.,0.,1.,1.]}];
     let mut gilrs_obj = gilrs::Gilrs::new();
-    let mut x = 0_f64;
-    let mut y = 0_f64;
     let mut window: PistonWindow =
-        WindowSettings::new("Hello Piston!", [640, 480])
+        WindowSettings::new("Hello Piston!", [window_size.0, window_size.1])
         .exit_on_esc(true).build().unwrap();
     while let Some(e) = window.next() {
         for _ in gilrs_obj.poll_events() {}
-        let (dx,dy) = gilrs_obj.gamepad(0).state().left_stick;
-        x += apply_deadzone(deadzone, dx as f64);
-        y -= apply_deadzone(deadzone, dy as f64);
-        window.draw_2d(&e, |c, g| {
-            clear([1.0; 4], g);
-            rectangle([1.0, 0.0, 0.0, 1.0], // red
-                      [0.0, 0.0, 10.0, 10.0],
-                      c.transform.trans(x,y), g);
-        });
+        for p in players.iter_mut() {
+            p.control(&gilrs_obj);
+            p.render(&e, &mut window);
+        }
     }
 }
