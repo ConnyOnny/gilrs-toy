094a24089c06e1e09b3b16a32e0466d2aa92395a
Constantin Berhard <git.mail.enormator@xoxy.net>
2016-09-07 22:55:24 +0200

    We taught them to kill, because we were afraid to teach them to love.

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,39 @@ use piston_window::*;
 
 struct Player {
     position: (f64,f64),
+    old_position: (f64,f64),
     gamepad_id: usize,
     deadzone: f64,
     color: [f32;4],
+    jump_button_was_pressed: bool,
 }
 
 impl Player {
+    fn new(position: (f64,f64), gamepad_id: usize, deadzone: f64, color: [f32;4]) -> Player {
+        Player {
+            position: position,
+            old_position: position,
+            gamepad_id: gamepad_id,
+            deadzone: deadzone,
+            color: color,
+            jump_button_was_pressed: false,
+        }
+    }
     fn control(&mut self, g : &gilrs::Gilrs) {
-        let (dx,dy) = g.gamepad(self.gamepad_id).state().left_stick;
-        self.position.0 += apply_deadzone(self.deadzone, dx as f64);
-        self.position.1 -= apply_deadzone(self.deadzone, dy as f64);
+        self.old_position = self.position;
+        let gps = g.gamepad(self.gamepad_id).state();
+        let (dx,dy) = gps.left_stick;
+        let mut multiplier = 1.;
+        if gps.btn_south {
+            if !self.jump_button_was_pressed {
+                self.jump_button_was_pressed = true;
+                multiplier = 100.;
+            }
+        } else {
+            self.jump_button_was_pressed = false;
+        }
+        self.position.0 += apply_deadzone(self.deadzone, dx as f64) * multiplier;
+        self.position.1 -= apply_deadzone(self.deadzone, dy as f64) * multiplier;
     }
     fn render(&self, e : &Event, w : &mut PistonWindow) {
         w.draw_2d(e, |c, g| {
@@ -41,8 +64,8 @@ fn main() {
     let window_size = (800,600);
     let deadzone = 0.2;
     let mut players = vec![
-        Player{position:(200.,200.),gamepad_id:0,deadzone:deadzone,color:[0.,0.,1.,1.]},
-        Player{position:(500.,300.),gamepad_id:1,deadzone:deadzone,color:[1.,0.,0.,1.]},
+        Player::new((200.,200.),0,deadzone,[0.,0.,1.,1.]),
+        Player::new((500.,300.),1,deadzone,[1.,0.,0.,1.]),
     ];
     let mut gilrs_obj = gilrs::Gilrs::new();
     assert!(gilrs_obj.connected_gamepad(0).is_some());
@@ -58,6 +81,25 @@ fn main() {
         for p in players.iter_mut() {
             p.control(&gilrs_obj);
             p.render(&e, &mut window);
+        }
+        let mut killed : Vec<usize> = Vec::new();
+        for p in players.iter() {
+            for q in players.iter() {
+                if p.gamepad_id == q.gamepad_id {
+                    continue;
+                }
+                let a = p.position.0 < q.position.0;
+                let b = p.old_position.0 < q.position.0;
+                let c = p.position.1 < q.position.1;
+                let d = p.old_position.1 < q.position.1;
+                if a != b && c != d {
+                    // jumped over
+                    killed.push(q.gamepad_id);
+                }
+            }
+        }
+        for k in killed.iter() {
+            players[*k].color = [0.,0.,0.,1.];
         }
     }
 }
