eb923600f2a5a7c3647468bc18a135324b428124
Constantin Berhard <git.mail.enormator@xoxy.net>
2016-09-22 01:32:16 +0200

    random colors, bigger window, minor improvements

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,18 @@ extern crate piston_window;
 extern crate rand;
 
 use piston_window::*;
+use rand::Rng;
+
+fn random_bright_color() -> [f32;4] {
+    let mut rng = rand::thread_rng();
+    let mut ret = [1.0, rng.gen_range(0.,1.), rng.gen_range(0.,1.), 1.0];
+    let swap_idx = rng.gen_range(0,3);
+    if swap_idx != 0 {
+        ret[0] = ret[swap_idx];
+        ret[swap_idx] = 1.0;
+    }
+    ret
+}
 
 struct Player {
     position: (f64,f64),
@@ -69,32 +81,43 @@ fn get_player_count(g : &gilrs::Gilrs) -> usize {
     i
 }
 
+fn reset_game(window: &mut PistonWindow, players: &mut Vec<Player>, e: &Event) {
+    window.draw_2d(e, |_, g| {
+        clear([0.0; 4], g);
+    });
+    players.iter_mut().map(|p| {
+            p.color = random_bright_color();
+        }).count(); // the count() is only to consume the iterator, so it actually iterates
+}
+
 fn main() {
-    let window_size = (800,600);
+    let window_size = (1700,900);
     let deadzone = 0.2;
     let mut gilrs_obj = gilrs::Gilrs::new();
     let mut players = Vec::new();
     for i in 0..get_player_count(&gilrs_obj) {
-        players.push(Player::new((100.,100.), i, deadzone, [1.0,0.,0.,1.0]));
+        players.push(Player::new(((200*(i+1)) as f64,100.), i, deadzone, [1.0,0.,0.,1.0]));
     }
     let mut window: PistonWindow =
         WindowSettings::new("Awesome Game", [window_size.0, window_size.1])
         .exit_on_esc(true).build().unwrap();
+    let mut is_init = false; // Maybe there is a nicer way to do this. I hope, there is.
     while let Some(e) = window.next() {
-        for _ in gilrs_obj.poll_events() {}
-        if gilrs_obj.gamepad(0).state().btn_east {
-
-        window.draw_2d(&e, |_, g| {
-            clear([1.0; 4], g);
-	        });
-	}
+        for _ in gilrs_obj.poll_events() {} // poll all events, so gilrs can update its internal state
+        if !is_init {
+            reset_game(&mut window, &mut players, &e);
+            is_init = true;
+        }
+        if players.iter().any(|p| gilrs_obj.gamepad(p.gamepad_id).state().btn_east) {
+            reset_game(&mut window, &mut players, &e);
+        }
         for p in players.iter_mut() {
             p.control(&gilrs_obj);
             p.render(&e, &mut window);
         }
         let mut killed : Vec<usize> = Vec::new();
         for p in players.iter() {
-            for q in players.iter() {
+            for (qi,q) in players.iter().enumerate() {
                 if p.gamepad_id == q.gamepad_id {
                     continue;
                 }
@@ -104,7 +127,7 @@ fn main() {
                 let d = p.old_position.1 < q.position.1;
                 if a != b && c != d {
                     // jumped over
-                    killed.push(q.gamepad_id);
+                    killed.push(qi);
                 }
             }
         }
